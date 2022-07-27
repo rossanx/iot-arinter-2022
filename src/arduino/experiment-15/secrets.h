25e2995e75cf2fa64dec405d5013882bd78774be
Rossano Pablo Pinto <rossano@gmail.com>
2022-07-26 21:15:46 -0300

    x

--- src/arduino/experiment-15/secrets.h
+++ src/arduino/experiment-15/secrets.h
@@ -1,4 +1,4 @@
 //#define SECRET_MAC {0x90, 0xA2, 0xDA, 0x10, 0x40, 0x4F}
 
-#define SECRET_CH_ID  1789011			// channel number
-#define SECRET_WRITE_APIKEY "RDV0HX0D5R4LU6JL"   // channel write API Key
+#define SECRET_CH_ID  00000000			// channel number
+#define SECRET_WRITE_APIKEY "YOUR API KEY HERE"   // channel write API Key
